1b3489eed156e0d5a1a7f055c38c003619ad20e6
GuilleOsc <guillermobianco9090@gmail.com>
2023-04-24 15:55:19 -0300

    Scripts para comprar producto

---
+++ tests/test-1.spec.ts
@@ -0,0 +1,10 @@
+import { test, expect } from '@playwright/test';
+
+test('test', async ({ page }) => {
+  await page.goto('https://www.mercadolibre.com.ar/');
+  await page.getByPlaceholder('Buscar productos, marcas y más…').click();
+  await page.getByPlaceholder('Buscar productos, marcas y más…').fill('iphone');
+  await page.getByPlaceholder('Buscar productos, marcas y más…').press('Enter');
+  await page.getByRole('link', { name: 'Apple iPhone 13 Pro (512 GB) - Plata' }).first().click();
+  await page.getByRole('button', { name: 'Comprar ahora' }).click();
+});
