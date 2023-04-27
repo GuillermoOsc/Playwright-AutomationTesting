8c7922fa0bce485b66434ea88bad4fb81c7ee7db
GuilleOsc <guillermobianco9090@gmail.com>
2023-04-27 01:00:43 -0300

    Localización de elementos con getByRole

--- tests/test-1.spec.ts
+++ tests/test-1.spec.ts
@@ -8,3 +8,15 @@ test('test', async ({ page }) => {
   await page.getByRole('link', { name: 'Apple iPhone 13 Pro (512 GB) - Plata' }).first().click();
   await page.getByRole('button', { name: 'Comprar ahora' }).click();
 });
+
+
+test('test locators', async ({ page }) => {
+
+  await page.goto('https://www.mercadolibre.com.ar')
+  await page.getByRole('link', { name: 'Ingresa', exact: true }).click()
+  await page.pause()
+
+});
+
+
+
